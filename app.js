a176457ad94b7532256a43025501548b32cd0a06
Angelika Jarosz <angelikajarosz@gmail.com>
2015-03-09 16:45:37 -0400

    list with a filter

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', ['$scope', function($scope){
     $scope.test = 'Hello World';
+    $scope.posts = [
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 1},
+        {title: 'post 3', upvotes: 4},
+        {title: 'post 4', upvotes: 6},
+        {title: 'post 5', upvotes: 2}
+    ];
 }]);
