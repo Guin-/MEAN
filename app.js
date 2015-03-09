80f87372906efad5e058c4a7e652956a366edc04
Angelika Jarosz <angelikajarosz@gmail.com>
2015-03-09 18:45:50 -0400

    make scope.posts a service

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', ['$scope', function($scope){
+app.factory('posts', [function(){
+    var o = {
+        posts: []
+    };
+    return o;
+}]);
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
     $scope.test = 'Hello World';
-    $scope.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 1},
-        {title: 'post 3', upvotes: 4},
-        {title: 'post 4', upvotes: 6},
-        {title: 'post 5', upvotes: 2}
-    ];
+    $scope.posts = posts.posts;
     $scope.addPost = function(){
         if(!$scope.title || $scope.title ===''){
             return;}
